989be480bf5bd8f4477b97dddf9efc465d5a6a58
Igor Maestri <igorpaiva@ucl.br>
2021-11-12 00:26:32 -0300

    Revert "Interface usuário"
    This reverts commit 4a8c6f0c687f09b708335ed1aede66678bdb8e53.

--- elevadorTrab2/Form1.Designer.cs
+++ elevadorTrab2/Form1.Designer.cs
@@ -31,10 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -43,9 +40,9 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(30, 86);
+            this.button1.Location = new System.Drawing.Point(36, 119);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(51, 47);
+            this.button1.Size = new System.Drawing.Size(75, 67);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -54,44 +51,20 @@
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(323, 254);
+            this.panel1.Location = new System.Drawing.Point(124, 147);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(111, 155);
+            this.panel1.Size = new System.Drawing.Size(150, 215);
             this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Location = new System.Drawing.Point(551, 205);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(111, 204);
-            this.panel2.TabIndex = 2;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.Location = new System.Drawing.Point(24, 92);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(61, 45);
-            this.button2.TabIndex = 0;
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 436);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(675, 436);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -100,8 +73,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button2;
     }
 }
 
