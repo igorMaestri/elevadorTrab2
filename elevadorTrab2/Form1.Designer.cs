a448bd58cd6ec6c4311df8c3123ca8d4251130e4
Igor Maestri <igorpaiva@ucl.br>
2021-11-14 13:26:48 -0300

    Lógica de acionamento, seta subindo/descendo, checkbox, tratamento de exceções, messagebox.

--- elevadorTrab2/Form1.Designer.cs
+++ elevadorTrab2/Form1.Designer.cs
@@ -58,6 +58,11 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.panel7 = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -153,7 +158,7 @@
             this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.fButton5);
-            this.panel3.Location = new System.Drawing.Point(433, 61);
+            this.panel3.Location = new System.Drawing.Point(314, 61);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(104, 157);
             this.panel3.TabIndex = 2;
@@ -193,7 +198,7 @@
             this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel4.Controls.Add(this.label1);
             this.panel4.Controls.Add(this.fButton4);
-            this.panel4.Location = new System.Drawing.Point(310, 25);
+            this.panel4.Location = new System.Drawing.Point(191, 25);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(109, 193);
             this.panel4.TabIndex = 3;
@@ -233,7 +238,7 @@
             this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel5.Controls.Add(this.label5);
             this.panel5.Controls.Add(this.fButton3);
-            this.panel5.Location = new System.Drawing.Point(186, 25);
+            this.panel5.Location = new System.Drawing.Point(433, 239);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(109, 193);
             this.panel5.TabIndex = 4;
@@ -271,6 +276,8 @@
             // 
             this.panel6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel6.BackgroundImage")));
             this.panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel6.Controls.Add(this.panel8);
+            this.panel6.Controls.Add(this.panel9);
             this.panel6.Controls.Add(this.dButton1);
             this.panel6.Controls.Add(this.textBox1);
             this.panel6.Controls.Add(this.dButton5);
@@ -304,14 +311,13 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Swis721 BlkOul BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.Red;
-            this.textBox1.Location = new System.Drawing.Point(33, 53);
+            this.textBox1.Location = new System.Drawing.Point(20, 53);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(62, 29);
+            this.textBox1.Size = new System.Drawing.Size(85, 36);
             this.textBox1.TabIndex = 8;
-            this.textBox1.Text = "0";
+            this.textBox1.Text = "1";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // dButton5
@@ -417,35 +423,38 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(6, 60);
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.checkBox1.Location = new System.Drawing.Point(3, 79);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.Size = new System.Drawing.Size(69, 17);
             this.checkBox1.TabIndex = 5;
             this.checkBox1.Text = "MANUAL";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            this.checkBox1.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(5, 122);
+            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.checkBox2.Location = new System.Drawing.Point(3, 108);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(97, 17);
+            this.checkBox2.Size = new System.Drawing.Size(95, 17);
             this.checkBox2.TabIndex = 6;
             this.checkBox2.Text = "AUTOMÁTICO";
             this.checkBox2.UseVisualStyleBackColor = true;
             this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
-            this.checkBox2.Click += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // panel7
             // 
             this.panel7.BackColor = System.Drawing.Color.Silver;
+            this.panel7.Controls.Add(this.label8);
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Controls.Add(this.label6);
             this.panel7.Controls.Add(this.checkBox2);
             this.panel7.Controls.Add(this.checkBox1);
-            this.panel7.Location = new System.Drawing.Point(435, 239);
+            this.panel7.Location = new System.Drawing.Point(438, 61);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(104, 193);
+            this.panel7.Size = new System.Drawing.Size(104, 157);
             this.panel7.TabIndex = 7;
             // 
             // timer1
@@ -453,11 +462,61 @@
             this.timer1.Interval = 2000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Selecione um modo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "de operação para";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "iniciar:";
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Transparent;
+            this.panel9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel9.BackgroundImage")));
+            this.panel9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel9.Location = new System.Drawing.Point(20, 53);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(24, 36);
+            this.panel9.TabIndex = 9;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Transparent;
+            this.panel8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel8.BackgroundImage")));
+            this.panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel8.Location = new System.Drawing.Point(81, 53);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(24, 36);
+            this.panel8.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 444);
+            this.ClientSize = new System.Drawing.Size(557, 444);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
@@ -515,6 +574,11 @@
         private System.Windows.Forms.Button dButton1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
     }
 }
 
--- elevadorTrab2/Form1.cs
+++ elevadorTrab2/Form1.cs
@@ -13,118 +13,246 @@ namespace elevadorTrab2
 {
     public partial class Form1 : Form
     {
-        int segundos;
-        int aux;
+        int segundos; //auxiliar timer
+        int aux=0; //auxiliar botoes
+        int auxFinal = 0; //auxiliar botoes
 
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();          
         }
 
-        private void dButton1_Click(object sender, EventArgs e)
+        
+
+        private void dButton1_Click(object sender, EventArgs e) //botao acionado
         {
-            timer1.Start();
-            aux = 1;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start(); //inicia a contagem do timer
+                aux = 1;                
+            }
+
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void dButton2_Click(object sender, EventArgs e)
         {
-            timer1.Start();            
-            aux = 2;
-        }                                    
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 2;
+                
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
+        }
 
         private void dButton3_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 3;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 3;
+               
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void dButton4_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 4;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 4;
+               
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void dButton5_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 5;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 5;
+              
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void fButton1_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 1;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 1;
+               
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void fButton2_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 2;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 2;
+                
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void fButton3_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 3;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 3;
+               
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void fButton4_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 4;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 4;
+               
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void fButton5_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            aux = 5;
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                timer1.Start();
+                aux = 5;
+               
+            }
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Selecione o modo de operação Manual");
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             segundos = 1;
             segundos--;
-            if (segundos == 0)
+            
+            if (segundos == 0 && checkBox1.Checked)
             {
-                if (aux == 1)
-                {
-                    timer1.Stop();
-                    textBox1.Text = "1"; //Alterando as propriedades do Objeto 
-                }
-                if (aux == 2)
-                {
-                    timer1.Stop();
-                    textBox1.Text = "2"; //Alterando as propriedades do Objeto 
-                }
-                if (aux == 3)
-                {
-                    timer1.Stop();
-                    textBox1.Text = "3"; //Alterando as propriedades do Objeto 
-                }
-                if (aux == 4)
-                {
-                    timer1.Stop();
-                    textBox1.Text = "4"; //Alterando as propriedades do Objeto 
-                }
-                if (aux == 5)
-                {
-                    timer1.Stop();
-                    textBox1.Text = "5"; //Alterando as propriedades do Objeto 
-                }
+                //if (aux > auxFinal)
+                //{
+                    //aux--;
+
+
+                    //if (aux < auxFinal)
+                    //{
+                        //aux++;
+
+
+                        //if (aux == auxFinal)
+                        //{
+
+
+
+
+                            if (aux == 1)
+                            {
+                                timer1.Stop();
+                                auxFinal = aux;
+                                textBox1.Text = "1"; //Alterando as propriedades do Objeto 
+                            }
+                            if (aux == 2)
+                            {
+                                timer1.Stop();
+                                auxFinal = aux;
+                                textBox1.Text = "2"; //Alterando as propriedades do Objeto 
+                                panel8.Visible = false;
+                            }
+                            if (aux == 3)
+                            {
+                                timer1.Stop();
+                                auxFinal = aux;
+                                textBox1.Text = "3"; //Alterando as propriedades do Objeto 
+                            }
+                            if (aux == 4)
+                            {
+                                timer1.Stop();
+                                auxFinal = aux;
+                                textBox1.Text = "4"; //Alterando as propriedades do Objeto 
+                            }
+                            if (aux == 5)
+                            {
+                                timer1.Stop();
+                                auxFinal = aux;
+                                textBox1.Text = "5"; //Alterando as propriedades do Objeto 
+                            }
+                        //}
+
+                    //}
+                //}
+            }
+            
+        }    
+        
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) //Manual
+        {
+            if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                MessageBox.Show("Elevador em modo Manual");
+            }
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                //checkBox2.CheckState = false;
+                //checkBox1.CheckState = false;
+                MessageBox.Show("Selecione apenas um modo de operação");
             }
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e) //Manual
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) //Automatico
         {
-
+            if(checkBox2.Checked && !checkBox1.Checked)
+            {
+                MessageBox.Show("Elevador em modo Automático"); 
+                //lógica automático
+            }
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                //checkBox2.CheckState = false;
+                //checkBox1.CheckState = false;
+                MessageBox.Show("Selecione apenas um modo de operação");
+            }
         }
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e) //Automático
-        {
-
-        }
-
-        
     }
 }
